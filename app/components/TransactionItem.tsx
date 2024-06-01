7d8982c532a6897ab9ef07dd10b3ab48b51b63db
Igor Milosavljevic <91330526+igor570@users.noreply.github.com>
2024-06-01 16:46:08 +0100

    added transaction item component

--- app/components/TransactionItem.tsx
+++ app/components/TransactionItem.tsx
@@ -1,3 +1,34 @@
-export const TransactionItem = () => {
-  return <div></div>;
+import { useFormatCurrency } from "@/hooks/useFormatCurrency";
+
+type TransactionItemProps = {
+  type?: string;
+  category?: string;
+  description?: string;
+  amount: number;
+};
+
+export const TransactionItem = ({
+  type,
+  category,
+  description,
+  amount,
+}: TransactionItemProps) => {
+  const formattedAmount = useFormatCurrency(amount);
+  return (
+    <div className="w-full flex items-center">
+      <div className="flex items-center mr-4 grow">
+        <span>{description}</span>
+      </div>
+
+      <div className="min-w-[150px] items-center hidden md:flex">
+        {category && (
+          <div className="rounded-md text-xs bg-gray-700 dark:bg-gray-100 text-gray-100 dark:text-black px-2 py-0.5">
+            {category}
+          </div>
+        )}
+      </div>
+      <div className="min-w-[150px] text-right">{formattedAmount}</div>
+      <div className="min-w-[50px] flex ">...</div>
+    </div>
+  );
 };
--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../components/Header";
 import { Trend } from "@/app/components/Trend";
+import { TransactionItem } from "../components/TransactionItem";
 
 const Page = () => {
   return (
@@ -19,6 +20,20 @@ const Page = () => {
         <hr className={"mb-4 border-gray-200 dark:border-gray-800"} />
         <div className={"flex space-x-4"}>
           <Trend type={"Income"} amount={1000} prevAmount={2000} />
+        </div>
+      </div>
+
+      <div>
+        <h2 className={"mb-4 text-lg font-mono"}>TransactionItem.tsx</h2>
+        <hr className={"mb-4 border-gray-200 dark:border-gray-800"} />
+        <div className="space-y-4">
+          <TransactionItem type="Income" description="Salary" amount={1000} />
+          <TransactionItem
+            type="Expense"
+            description="Food"
+            category="Food"
+            amount={50}
+          />
         </div>
       </div>
     </main>
